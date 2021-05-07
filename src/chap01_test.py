fffe4f3c4b4c6c1e6705baf2d5556e64135cdfda
k2works <kakimomokuri@gmail.com>
2021-05-07 10:30:13 +0900

    refactor: 関数のインライン化

--- src/chap01_test.py
+++ src/chap01_test.py
@@ -313,14 +313,7 @@ def sum_1_to_positive(n):
         if n > 0:
             break
 
-    sum = 0
-    i = 1
-
-    for i in range(1, n + 1):
-        sum += i
-        i += 1
-
-    return sum
+    return sum(range(1, n + 1))
 
 doctest.testmod(verbose=True)
 unittest.main(argv=[''], verbosity=2, exit=False)
