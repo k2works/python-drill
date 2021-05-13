bebee32497ce4b4a7d449a7bcfcd69af13e1aa56
k2works <kakimomokuri@gmail.com>
2021-05-13 10:46:46 +0900

    refactor: 説明変数の導入

--- src/chap01_test.py
+++ src/chap01_test.py
@@ -406,14 +406,14 @@ def multiplication_table():
     >>> multiplication_table()
     '---------------------------\\n  1  2  3  4  5  6  7  8  9\\n  2  4  6  8 10 12 14 16 18\\n  3  6  9 12 15 18 21 24 27\\n  4  8 12 16 20 24 28 32 36\\n  5 10 15 20 25 30 35 40 45\\n  6 12 18 24 30 36 42 48 54\\n  7 14 21 28 35 42 49 56 63\\n  8 16 24 32 40 48 56 64 72\\n  9 18 27 36 45 54 63 72 81\\n---------------------------'
     """
-    header = ''.join(['-' for _ in range(27)])
-    result = header + '\n'
+    header = ''.join(['-' for _ in range(27)]) + '\n'
 
     def row(i): return ''.join([f'{i * j:3}' for j in range(1, 10)]) + '\n'
-    result += ''.join([row(i) for i in range(1, 10)])
+    body = ''.join([row(i) for i in range(1, 10)])
 
     footer = ''.join(['-' for _ in range(27)])
-    result += footer
+
+    result = header + body + footer
     return result
 
 doctest.testmod(verbose=True)
