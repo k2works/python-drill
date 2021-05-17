277932e51003fa07c5eefd56afd5fc6a92a96e55
k2works <kakimomokuri@gmail.com>
2021-05-17 10:07:26 +0900

    feat: 配列の必要性

--- src/chap02_test.py
+++ src/chap02_test.py
@@ -3,6 +3,7 @@
 # %%
 import doctest
 import unittest
+from unittest import result
 from unittest.case import skip
 
 # %% [markdown]
@@ -14,6 +15,24 @@ from unittest.case import skip
 class TestTotal(unittest.TestCase):
     def test_5人の点数を読み込んで合計点平均点を返す(self):
         self.assertEqual(total(32, 68, 72, 54, 92), '318,63.6')
+
+def total(tensu1, tensu2, tensu3, tensu4, tensu5):
+    """ 5人の点数を読み込んで合計点平均点を返す
+
+    >>> total(32,68,72,54,92)
+    '318,63.6'
+    """
+    result = ''
+    total = 0
+    total += tensu1
+    total += tensu2
+    total += tensu3
+    total += tensu4
+    total += tensu5
+    result = str(total)
+    result += ','
+    result += str(total/5)
+    return result
 
 
 # %% [markdown]
