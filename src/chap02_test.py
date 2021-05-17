61bd582ae60476b1cc408fb3b8001333e94197d6
k2works <kakimomokuri@gmail.com>
2021-05-17 10:02:47 +0900

    test: 配列の必要性

--- src/chap02_test.py
+++ src/chap02_test.py
@@ -9,4 +9,15 @@ from unittest.case import skip
 # ## データ構造と配列
 
 # %% [markdown]
+# ### 配列の必要性
+
+class TestTotal(unittest.TestCase):
+    def test_5人の点数を読み込んで合計点平均点を返す(self):
+        self.assertEqual(total(32, 68, 72, 54, 92), '318,63.6')
+
+
+# %% [markdown]
 # ## 配列
+
+doctest.testmod(verbose=True)
+unittest.main(argv=[''], verbosity=2, exit=False)
