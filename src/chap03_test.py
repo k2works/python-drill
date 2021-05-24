78807095c427db517abd1b47a4e6d160f52df51a
k2works <kakimomokuri@gmail.com>
2021-05-24 10:08:56 +0900

    docs: 探索

---
+++ src/chap03_test.py
@@ -0,0 +1,15 @@
+
+# %% [markdown]
+# # 探索
+# %%
+import doctest
+import unittest
+from unittest import result
+from unittest.case import skip
+from typing import Any, MutableSequence, Sequence
+
+# %% [markdown]
+# ## 線形探索
+
+# %% [markdown]
+# ### 線形探索
