c1fc660f1cc7ad9b956bb12fc5474f0af870d1ff
k2works <kakimomokuri@gmail.com>
2021-05-24 10:17:06 +0900

    test: 線形探索

--- src/chap03_test.py
+++ src/chap03_test.py
@@ -17,6 +17,9 @@ from typing import Any, MutableSequence, Sequence
 class TestSearch(unittest.TestCase):
     def test_シーケンスaからkeyと等価な要素を線形探索_1(self):
         self.assertEqual(ssearch_while([6, 4, 3, 2, 1, 2, 8], 2), 3)
+        self.assertEqual(ssearch_while([12.7, 3.14, 6.4, 7.2, 'End'], 6.4), 2)
+        self.assertEqual(ssearch_while((4, 7, 5.6, 2, 3.14, 1), 5.6), 2)
+        self.assertEqual(ssearch_while(['DTS', 'AAC', 'FLAC'], 'DTS'), 0)
 
 def ssearch_while(a: Sequence, key: Any) -> int:
     """シーケンスaからkeyと等価な要素を線形探索(while文)
