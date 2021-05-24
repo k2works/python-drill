d9da04ee33f43b68e8e03672cfae43d3f000d5db
k2works <kakimomokuri@gmail.com>
2021-05-24 10:11:03 +0900

    test: 線形探索

--- src/chap03_test.py
+++ src/chap03_test.py
@@ -13,3 +13,7 @@ from typing import Any, MutableSequence, Sequence
 
 # %% [markdown]
 # ### 線形探索
+
+class TestSearch(unittest.TestCase):
+    def test_シーケンスaからkeyと等価な要素を線形探索_1(self):
+        self.assertEqual(search_while([6, 4, 3, 2, 1, 2, 8], 2), 3)
