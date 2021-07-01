1efb5f0f0b65378b9551eff653d23c6eb6e1c384
k2works <kakimomokkuri@gmail.com>
2021-07-01 10:28:44 +0900

    test: スタック

--- src/chap04_test.py
+++ src/chap04_test.py
@@ -1,9 +1,34 @@
+import unittest
+from typing import Any
+
 # %% [markdown]
 # # スタックとキュー
 # %%
 
 # %% [markdown]
 # ## スタック
+
+
+class TestFixedStack(unittest.TestCase):
+    def test_push(self):
+        s = FixedStack(64)
+        s.push(1)
+        self.assertEqual(s.dump(), [1])
+
+
+class FixedStack:
+    def __init__(self, capacity: int = 256) -> None:
+        self.stk = [None] * capacity
+        self.capacity = capacity
+        self.ptr = 0
+
+    def push(self, value: Any) -> None:
+        self.stk[self.ptr] = value
+        self.ptr += 1
+
+    def dump(self) -> None:
+        return self.stk[:self.ptr]
+
 
 # %% [markdown]
 # ### 固定長スタック
