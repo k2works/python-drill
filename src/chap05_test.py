3c62884dad0b3ad8dd22cc95cce933057be849fe
k2works <kakimomokkuri@gmail.com>
2021-07-12 10:07:02 +0900

    feat: 再帰の基本

--- src/chap05_test.py
+++ src/chap05_test.py
@@ -26,8 +26,29 @@ def factorial(n: int) -> int:
     else:
         return 1
 
+
+class TestGcd(unittest.TestCase):
+    def test_22と8の最大公約数は2であること(self):
+        self.assertEqual(gcd(22, 8), 2)
+
+
+def gcd(x: int, y: int) -> int:
+    """
+    ユークリッドの互除法を用いて、xとyの最大公約数を求める。
+    >>> gcd(22, 8)
+    2
+    """
+    if y == 0:
+        return x
+    else:
+        return gcd(y, x % y)
+
 # %% [markdown]
 # ## ハノイの塔
 
 # %% [markdown]
 # ## 8王妃問題
+
+
+doctest.testmod(verbose=True)
+unittest.main(argv=[''], verbosity=2, exit=False)
