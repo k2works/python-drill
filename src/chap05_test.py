f2e203efb0f268e5c199d07f82066a520a993da8
k2works <kakimomokkuri@gmail.com>
2021-07-12 10:02:14 +0900

    feat: 再帰の基本

--- src/chap05_test.py
+++ src/chap05_test.py
@@ -9,6 +9,23 @@ from typing import Any
 # %% [markdown]
 # ## 再帰の基本
 
+
+class TestFactorial(unittest.TestCase):
+    def test_3の階乗は6であること(self):
+        self.assertEqual(factorial(3), 6)
+
+
+def factorial(n: int) -> int:
+    """
+    非負の整数の階乗値を求める
+    >>> factorial(3)
+    6
+    """
+    if n > 0:
+        return n * factorial(n - 1)
+    else:
+        return 1
+
 # %% [markdown]
 # ## ハノイの塔
 
