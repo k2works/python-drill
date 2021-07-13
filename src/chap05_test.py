d1156906c285bf3287ee7d1728caa4d615410e53
k2works <kakimomokkuri@gmail.com>
2021-07-13 10:36:23 +0900

    feat: 真に再帰的な関数

--- src/chap05_test.py
+++ src/chap05_test.py
@@ -1,6 +1,7 @@
 import unittest
 import doctest
 from typing import Any
+from chap04_test import Stack
 
 # %% [markdown]
 # # 再帰的アルゴリズム
@@ -42,6 +43,66 @@ def gcd(x: int, y: int) -> int:
         return x
     else:
         return gcd(y, x % y)
+
+
+class TestRecur(unittest.TestCase):
+    def test_実行結果を配列で返すこと(self):
+        self.assertEqual(recur(4, []), [1, 2, 3, 1, 4, 1, 2])
+
+    def test_末尾再帰を除去した実行結果を配列で返すこと(self):
+        self.assertEqual(recur2(4, []), [1, 2, 3, 1, 4, 1, 2])
+
+    def test_スタックを用いて末尾再帰を除去した実行結果を配列で返すこと(self):
+        self.assertEqual(recur3(4, []), [1, 2, 3, 1, 4, 1, 2])
+
+
+def recur(n: int, result: list) -> list:
+    """真に再帰的な関数
+
+    >>> recur(4, [])
+    [1, 2, 3, 1, 4, 1, 2]
+    """
+    if n > 0:
+        recur(n - 1, result)
+        result.append(n)
+        recur(n - 2, result)
+
+    return result
+
+def recur2(n: int, result: list) -> list:
+    """末尾再帰を除去した関数
+
+    >>> recur2(4, [])
+    [1, 2, 3, 1, 4, 1, 2]
+    """
+    while n > 0:
+        recur(n - 1, result)
+        result.append(n)
+        n = n - 2
+
+    return result
+
+def recur3(n: int, result: list) -> list:
+    """スタックを用いて末尾再帰を除去した関数
+
+    >>> recur3(4, [])
+    [1, 2, 3, 1, 4, 1, 2]
+    """
+    s = Stack(n)
+
+    while True:
+        if n > 0:
+            s.push(n)
+            n = n - 1
+            continue
+        if not s.is_empty():
+            n = s.pop()
+            result.append(n)
+            n = n - 2
+            continue
+        break
+
+    return result
 
 # %% [markdown]
 # ## ハノイの塔
