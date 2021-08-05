d3e344b5f486378b34a1ecb565e11c256f947b29
k2works <kakimomokkuri@gmail.com>
2021-08-05 10:08:56 +0900

    test: 単純交換ソート

--- src/chap06_test.py
+++ src/chap06_test.py
@@ -8,22 +8,43 @@ import doctest
 # %% [markdown]
 # ## 単純交換ソート（バブルソート）
 
-# %% [markdown]
-# ## 単純選択ソート
+
 class TestBubbleSort(unittest.TestCase):
     def test_bubble_sort(self):
-        array = [6,4,3,7,1,9,8]
+        array = [6, 4, 3, 7, 1, 9, 8]
         bubule_sort(array)
-        self.assertEqual(array, [1,3,4,6,7,8,9])
+        self.assertEqual(array, [1, 3, 4, 6, 7, 8, 9])
+
+    def test_bubble_sort2(self):
+        array = [6, 4, 3, 7, 1, 9, 8]
+        bubule_sort2(array)
+        self.assertEqual(array, [1, 3, 4, 6, 7, 8, 9])
+
 
 def bubule_sort(a: MutableSequence):
     """ 単純交換ソート"""
     n = len(a)
     for i in range(n-1):
-        for j in range(n-1,i,-1):
+        for j in range(n-1, i, -1):
             if a[j - 1] > a[j]:
                 a[j - 1], a[j] = a[j], a[j - 1]
 
+
+def bubule_sort2(a: MutableSequence):
+    """ 単純交換ソート (第２版：交換回数による打ち切り）"""
+    n = len(a)
+    for i in range(n-1):
+        exchange = 0
+        for j in range(n-1, i, -1):
+            if a[j - 1] > a[j]:
+                a[j - 1], a[j] = a[j], a[j - 1]
+                exchange += 1
+        if exchange == 0:
+            break
+
+
+# %% [markdown]
+# ## 単純選択ソート
 
 # %% [markdown]
 # ## 単純挿入ソート
