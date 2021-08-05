9c3cca9fa072309bee5b5e649ae15b41e0bc2327
k2works <kakimomokkuri@gmail.com>
2021-08-05 10:12:18 +0900

    test: 単純交換ソート

--- src/chap06_test.py
+++ src/chap06_test.py
@@ -12,16 +12,21 @@ import doctest
 class TestBubbleSort(unittest.TestCase):
     def test_bubble_sort(self):
         array = [6, 4, 3, 7, 1, 9, 8]
-        bubule_sort(array)
+        bubble_sort(array)
         self.assertEqual(array, [1, 3, 4, 6, 7, 8, 9])
 
     def test_bubble_sort2(self):
         array = [6, 4, 3, 7, 1, 9, 8]
-        bubule_sort2(array)
+        bubble_sort2(array)
+        self.assertEqual(array, [1, 3, 4, 6, 7, 8, 9])
+
+    def test_bubble_sort3(self):
+        array = [6, 4, 3, 7, 1, 9, 8]
+        bubble_sort3(array)
         self.assertEqual(array, [1, 3, 4, 6, 7, 8, 9])
 
 
-def bubule_sort(a: MutableSequence):
+def bubble_sort(a: MutableSequence):
     """ 単純交換ソート"""
     n = len(a)
     for i in range(n-1):
@@ -30,7 +35,7 @@ def bubule_sort(a: MutableSequence):
                 a[j - 1], a[j] = a[j], a[j - 1]
 
 
-def bubule_sort2(a: MutableSequence):
+def bubble_sort2(a: MutableSequence):
     """ 単純交換ソート (第２版：交換回数による打ち切り）"""
     n = len(a)
     for i in range(n-1):
@@ -41,6 +46,18 @@ def bubule_sort2(a: MutableSequence):
                 exchange += 1
         if exchange == 0:
             break
+
+def bubble_sort3(a: MutableSequence):
+    """ 単純選択ソート（第3版：捜査範囲を限定）"""
+    n = len(a)
+    k = 0
+    while k < n - 1:
+        last = n - 1
+        for j in range(n - 1, k, -1):
+            if a[j - 1] > a[j]:
+                a[j - 1], a[j] = a[j], a[j - 1]
+                last = j
+        k = last
 
 
 # %% [markdown]
